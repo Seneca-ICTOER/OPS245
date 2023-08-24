3998b72dde529f3a43e4ffba43d3de2b9b4a8ebb
Chris Johnson <chris.johnson@senecacollege.ca>
2023-08-24 10:06:55 -0400

    Preliminary mod for new course code.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,19 +5,19 @@ const katex = require('rehype-katex');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'OER Course Title',
-  tagline: 'OER Tag/Keyword',
+  title: 'OPS245 Course Content - Dev Space for Fall 2023',
+  tagline: 'OPS245',
   url: 'https://Seneca-ICTOER.github.io/',
-  baseUrl: '/OERTemplate/',
+  baseUrl: '/OPS245dev/',
   trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'Seneca-ICTOER',
-  projectName: 'OERTemplate',
+  projectName: 'OPS245dev',
   themeConfig: {
     navbar: {
-      title: 'OER Course Title',
+      title: 'OPS245 Course Content - Dev Space for Fall 2023',
       logo: {
         alt: 'Seneca College',
         src: 'img/logo.svg',
@@ -31,7 +31,7 @@ module.exports = {
       style: 'dark',
       links: [
         {
-          title: 'OER Course Title',
+          title: 'OPS245 Course Content - Dev Space for Fall 2023',
           items: [
             {
               label: 'Contents',
@@ -57,7 +57,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
-          editUrl: 'https://github.com/Seneca-ICTOER/OERTemplate/tree/main',
+          editUrl: 'https://github.com/Seneca-ICTOER/OPS245dev/tree/main',
           remarkPlugins: [math],
           rehypePlugins: [katex],
         },
